2a1ecaaa875e65d4dc5b8a3914330cae64f9450a
JC Redmond III <jcredmond1516@gmail.com>
2024-12-27 00:28:33 +0000

    make build dir root dir so can server index.html

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  build: {
+    outDir: '..'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
